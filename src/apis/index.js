dfa3191b78ccdf6685250b28d9584af116425552
TemoSulava <sulava.temo@yahoo.com>
2022-09-01 00:56:42 +0400

    added apis object

---
+++ src/apis/index.js
@@ -0,0 +1,7 @@
+ const projectApis = {
+  products: 'https://cms.meamacollect.ge/meama-collect/api/client/ka',
+  supportedLanguages: 'https://cms.meamacollect.ge/meama-collect/api/client/languages',
+  contactInfo: 'https://cms.meamacollect.ge/meama-collect/api/client/ka/contact-info'
+}
+
+export default projectApis
