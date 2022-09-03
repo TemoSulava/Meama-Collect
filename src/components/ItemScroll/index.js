1c88b51943e600e883dd4b995c68389a6b238bf2
TemoSulava <sulava.temo@yahoo.com>
2022-09-03 19:00:22 +0400

    layed ground to start rendering the data from the products api according to the design

---
+++ src/components/ItemScroll/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import ScrollContainer from 'react-indiana-drag-scroll'
+
+const ItemScroll = ({data}) => {
+  return (
+     <ScrollContainer vertical className="scroll-container">
+        {/*map data here*/}
+      </ScrollContainer>
+  )
+}
+
+export default ItemScroll
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Layout } from 'antd'
 
+import projectApis from '../../apis'
+
 import LanguageSelector from '../../applets/LanguageSelector'
+
+import useFetch from '../../customHooks/useFetch'
 
 import SvgLoader from '../../components/SvgLoader'
 
@@ -9,6 +13,19 @@ import SvgLoader from '../../components/SvgLoader'
 const { Header, Footer, Content } = Layout
 
 const Main = () => {
+
+  const { data, loading, error } = useFetch(projectApis.PRODUCTS)
+
+     if (error) console.error(error)
+
+     if (loading) return <div>Placeholder loading text...</div>
+
+     const generalData = data && data[0]
+
+     //TODO 1: Use general data in the header along with 3 cards to render vertically.
+     //TODO: 2: use rest of the data to render as rows according to the titles in the content body
+     //TODO 3(optional): Create filter to filter for the subCategories where accessable
+
   return (
     <div className='main-component'>
       <Layout>
