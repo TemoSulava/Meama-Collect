cb90a715ff7e74d4e0d61fa8fbfacd4d6fd802ac
TemoSulava <sulava.temo@yahoo.com>
2022-09-05 01:49:54 +0400

    TODO: refactor all the ItemScrolls inside of a single map function

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -11,6 +11,8 @@ import SvgLoader from '../../components/SvgLoader'
 
 import ItemScroll from '../../components/ItemScroll'
 
+import './index.css'
+
 const { Header, Footer, Content } = Layout
 
 const Main = () => {
@@ -27,24 +29,23 @@ const Main = () => {
 
   const teaData = data[3].subCategories[0]
 
-  console.log(teaData)
+  console.log(data)
   //TODO 3(optional): Create filter to filter for the subCategories where accessable
 
   return (
     <div className='main-component'>
-      <Layout>
+      <Layout >
         <Header className='grid-item-1' style={{ background: '#000000' }}>
           <SvgLoader className='site-logo' type='meama-logo' />
           <SvgLoader className='header-bg' type='header-svg' />
           <LanguageSelector />
           <ItemScroll data={coffeeData} style={{ marginTop: '50px' }} title='ყავის მენიუ' />
         </Header>
-        <Content className='content-item'>
-          <ItemScroll data={teaData} marginLeft = '40px' style={{ marginTop: '200px' }} title='ჩაი' titleColor='#00000' />
+        <Content  className='content-item'>
+          <ItemScroll data={teaData} marginLeft='40px' style={{ marginTop: '200px' }} title='ჩაი' titleColor='#00000' />
+          {/*render all items refactor all the items inside of a single map*/}
         </Content>
-        <Footer className='grid-item-3' style={{ background: 'grey' }}>
-          Footer
-        </Footer>
+        
       </Layout>
     </div>
   )
